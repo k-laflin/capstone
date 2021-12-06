1effabc5511b2b7cf575093d6b2c40ad613b7abe
k-laflin <kellie.laflin@slalom.com>
2021-12-06 16:04:21 -0500

    add rollback step on failure

--- update-bashrc.sh
+++ update-bashrc.sh
@@ -5,7 +5,7 @@ add() { n="$@"; bc <<< "${n// /+}"; }
 
 #get most recent capstone version as a number
 export VERSION=$(aws ecr describe-images --repository-name capstone \
---query 'sort_by(imageDetails,& imagePushedAt)[-1].imageTags[0]' --output text | tr -d v)
+--query 'sort_by(imageDetails,& imagePushedAt)[*].imageTags[0]' --output text | xargs -n1 | sort | xargs | cut -d" " -f4 | tr -d v)
 #index version automatically 
 export INDEX_VERSION=$(add $VERSION 0.1)
 #create new version 
