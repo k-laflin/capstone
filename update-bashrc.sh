086833d5de895d7aaa1031b6dd7e9908f4fc1663
k-laflin <kellie.laflin@slalom.com>
2021-12-07 11:55:20 -0500

    fix deployment & rollback steps

--- update-bashrc.sh
+++ update-bashrc.sh
@@ -5,7 +5,7 @@ add() { n="$@"; bc <<< "${n// /+}"; }
 
 #get most recent capstone version as a number
 export VERSION=$(aws ecr describe-images --repository-name capstone \
---query 'sort_by(imageDetails,& imagePushedAt)[*].imageTags[0]' --output text | xargs -n1 | sort | xargs | cut -d" " -f4 | tr -d v)
+--query 'sort_by(imageDetails,& imagePushedAt)[*].imageTags' --output text | xargs -n1 | sort | xargs | rev | cut -d" " -f1 | rev | tr -d v)
 #index version automatically 
 export INDEX_VERSION=$(add $VERSION 0.1)
 #create new version 
