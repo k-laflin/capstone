00ad3f3bc2cb3d3fcbe930350874f70948dd69de
k-laflin <kellie.laflin@slalom.com>
2021-12-07 14:39:25 -0500

    adding linting failure for screenshot

--- app.py
+++ app.py
@@ -6,11 +6,9 @@ from flask import render_template
 R = random.randint(0, 255)
 G = random.randint(0, 255)
 B = random.randint(0, 255)
-#rgb = [r,g,b]
-#print('A Random color is :',rgb)
 
 #create new background html
-hello = """
+HELLO = """
     <html style="background-color:rgb({}, {}, {});">
     <head>
     <title>Flask Tutorial</title>
@@ -21,15 +19,15 @@ hello = """
     </html>  
 """.format(R, G, B)
 
-print(hello)
+print(HELLO)
 
 INDEX = open('templates/index.html', 'w')
-INDEX.write(hello)
+INDEX.write(HELLO)
 INDEX.close()
 
 APP = Flask(__name__)
 
 @APP.route('/')
 def index():
-    return render_template('index.html')
+    #return render_template('index.html')
 APP.run(host='0.0.0.0', port=81)
