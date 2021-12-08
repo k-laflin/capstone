10f4ab8e0256564122359a300e382c9abefd03ba
k-laflin <kellie.laflin@slalom.com>
2021-12-08 12:57:09 -0500

    fix cluster shell script

--- cluster-setup/build-cluster.sh
+++ cluster-setup/build-cluster.sh
@@ -1,14 +1,15 @@
 #set variables
-export $AWS_REGION='us-west-2'
-export $ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+export AWS_REGION='us-west-2'
+export ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 
 #******************#
 #eks cluster
 #******************#
 #go to folder
-cd cluster-setup
+ls
 
 #create cluster
+echo 'creating capstone cluster'
 eksctl create cluster -f capstone-cluster.yml
 
 #******************#
@@ -17,11 +18,13 @@ eksctl create cluster -f capstone-cluster.yml
 cd manifests/alb-controller
 
 #create OIDC identity controller
+echo 'creating iam oidc provider'
 eksctl utils associate-iam-oidc-provider \
     --region ${AWS_REGION} \
     --cluster capstone \
     --approve
 
+echo 'creating iam service account'
 eksctl create iamserviceaccount \
     --cluster capstone \
     --namespace kube-system \
@@ -31,18 +34,22 @@ eksctl create iamserviceaccount \
     --approve
 
 #install cert-manager
+echo 'installing cert-manager'
 kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.4.1/cert-manager.yaml
 
 #create load balancer
+echo 'create load balancer'
 kubectl apply -f v2_2_1_full.yaml
 
 #******************#
 #deploy microservice
 #******************#
 #go to manifest folder
-cd ~/cluster-setup/manifests
+cd ..
+ls
 
 #deploy app
+echo 'deploy app'
 kubectl apply -f deploy-app.yml
 kubectl apply -f service-app.yml
 kubectl apply -f ingress.yml
