740423cffdd482bf743a41feaea0fc9646e2f48f
k-laflin <kellie.laflin@slalom.com>
2021-12-09 12:54:33 -0500

    first appdev commit

--- cluster-setup/build-cluster.sh
+++ cluster-setup/build-cluster.sh
@@ -55,3 +55,23 @@ kubectl apply -f service-app.yml
 kubectl apply -f ingress.yml
 
 
+# 'apiVersion: v1
+#   6 data:
+#   7   mapUsers: |
+#   8     - userarn: arn:aws:iam::466390023253:user/Admin
+#   9       username: admin
+#  10       groups: 
+#  11         - system:masters
+#  12   mapRoles: |
+#  13     - groups:
+#  14       - system:bootstrappers
+#  15       - system:nodes
+#  16       rolearn: arn:aws:iam::466390023253:role/eksctl-capstone-nodegroup-capston-NodeInstanceRole-EPCVKJT724SC
+#  17       username: system:node:{{EC2PrivateDNSName}}
+#  18 kind: ConfigMap
+#  19 metadata:
+#  20   creationTimestamp: "2021-12-08T19:53:20Z"
+#  21   name: aws-auth
+#  22   namespace: kube-system
+#  23   resourceVersion: "1420"
+#  24   uid: 52baa22a-d3f5-4144-bb50-59127cad79c0'
